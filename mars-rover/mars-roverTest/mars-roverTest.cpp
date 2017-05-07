580a8773c4ce7146ab294e9b7b57b690866b23d9
Dani <dani.robles@gmail.com>
2017-05-07 18:04:44 +0200

    added test fixture

--- mars-rover/mars-roverTest/mars-roverTest.cpp
+++ mars-rover/mars-roverTest/mars-roverTest.cpp
@@ -5,19 +5,59 @@
 #include "gtest/gtest.h"
 #include "mars-rover.h"
 
+namespace 
+{
+
+// The fixture for testing class MarsRover.
+class MarsRoverTest : public ::testing::Test 
+{
+protected:
+	Point startingPoint;
+	Direction startingDirection;
+	MarsRover* theRover;
+
+
+	MarsRoverTest() {
+		// You can do set-up work for each test here.
+
+		Direction startingDirectionToTheNorth(1, 0, 0, 0);
+		startingDirection = startingDirectionToTheNorth;
+
+		theRover = new MarsRover(startingPoint, startingDirection);
+	}
+
+	virtual ~MarsRoverTest() {
+		// You can do clean-up work that doesn't throw exceptions here.
+		delete theRover;
+	}
+
+	// If the constructor and destructor are not enough for setting up
+	// and cleaning up each test, you can define the following methods:
+
+	virtual void SetUp() {
+		// Code here will be called immediately after the constructor (right
+		// before each test).
+	}
+
+	virtual void TearDown() {
+		// Code here will be called immediately after each test (right
+		// before the destructor).
+	}
+};
+
+
 // You are given the initial starting point (x,y) of a rover and the direction
 // (N,S,E,W) it is facing.
 
-TEST(testMarsRover, initialSetup)
+TEST_F(MarsRoverTest, initialSetup)
 {
-	Point startingPoint;
-	Direction startingDirectionToTheNorth(1, 0, 0, 0);
-	
-	MarsRover theRover(startingPoint, startingDirectionToTheNorth);
-
-	bool result = (theRover.getStartingPoint() == startingPoint);
+	bool result = (theRover->getStartingPoint() == startingPoint);
 	EXPECT_EQ(result, true);
 
-	result = (theRover.getStartingDirection() == startingDirectionToTheNorth);
+	result = (theRover->getStartingDirection() == startingDirection);
 	EXPECT_EQ(result, true);
 }
+
+
+
+}  // namespace
