6f5f7873262434a63b5227c4d1159b9a7d1c2e8c
Dani <dani.robles@gmail.com>
2017-05-21 16:23:24 +0200

    create Player class

--- Tennis2/Tennis2/tennis2.cpp
+++ Tennis2/Tennis2/tennis2.cpp
@@ -1,87 +1,138 @@
 #include <string>
 
+
+class Player
+{
+public:
+
+	Player(int score)
+	{
+		score_ = score;
+	}
+
+	int getPoints()
+	{
+		return score_;
+	}
+
+
+private:
+
+	int score_;
+
+};
+
+class Game
+{
+public:
+
+	Game(Player* playerOne, Player* playerTwo)
+	{
+		playerOne_ = playerOne;
+		playerTwo_ = playerTwo;
+	}
+
+	std::string getScore()
+	{
+
+	}
+
+
+private:
+
+	Player* playerOne_;
+	Player* playerTwo_;
+
+
+};
+
 const std::string tennis_score(int p1Score, int p2Score) {
 	std::string score = "";
 	std::string P1res = "";
 	std::string P2res = "";
-	if (p1Score == p2Score && p1Score < 4)
+
+	Player playerOne(p1Score);
+	Player playerTwo(p2Score);
+
+
+	if (playerOne.getPoints() == playerTwo.getPoints() && playerOne.getPoints() < 4)
 	{
-		if (p1Score == 0)
+		if (playerOne.getPoints() == 0)
 			score = "Love";
-		if (p1Score == 1)
+		if (playerOne.getPoints() == 1)
 			score = "Fifteen";
-		if (p1Score == 2)
+		if (playerOne.getPoints() == 2)
 			score = "Thirty";
 		score += "-All";
 	}
-	if (p1Score == p2Score && p1Score>2)
+	if (playerOne.getPoints() == playerTwo.getPoints() && playerOne.getPoints()>2)
 		score = "Deuce";
 
-	if (p1Score > 0 && p2Score == 0)
+	if (playerOne.getPoints() > 0 && playerTwo.getPoints() == 0)
 	{
-		if (p1Score == 1)
+		if (playerOne.getPoints() == 1)
 			P1res = "Fifteen";
-		if (p1Score == 2)
+		if (playerOne.getPoints() == 2)
 			P1res = "Thirty";
-		if (p1Score == 3)
+		if (playerOne.getPoints() == 3)
 			P1res = "Forty";
 
 		P2res = "Love";
 		score = P1res + "-" + P2res;
 	}
-	if (p2Score > 0 && p1Score == 0)
+	if (playerTwo.getPoints() > 0 && playerOne.getPoints() == 0)
 	{
-		if (p2Score == 1)
+		if (playerTwo.getPoints() == 1)
 			P2res = "Fifteen";
-		if (p2Score == 2)
+		if (playerTwo.getPoints() == 2)
 			P2res = "Thirty";
-		if (p2Score == 3)
+		if (playerTwo.getPoints() == 3)
 			P2res = "Forty";
 
 		P1res = "Love";
 		score = P1res + "-" + P2res;
 	}
 
-	if (p1Score>p2Score && p1Score < 4)
+	if (playerOne.getPoints()>playerTwo.getPoints() && playerOne.getPoints() < 4)
 	{
-		if (p1Score == 2)
+		if (playerOne.getPoints() == 2)
 			P1res = "Thirty";
-		if (p1Score == 3)
+		if (playerOne.getPoints() == 3)
 			P1res = "Forty";
-		if (p2Score == 1)
+		if (playerTwo.getPoints() == 1)
 			P2res = "Fifteen";
-		if (p2Score == 2)
+		if (playerTwo.getPoints() == 2)
 			P2res = "Thirty";
 		score = P1res + "-" + P2res;
 	}
-	if (p2Score>p1Score && p2Score < 4)
+	if (playerTwo.getPoints()>playerOne.getPoints() && playerTwo.getPoints() < 4)
 	{
-		if (p2Score == 2)
+		if (playerTwo.getPoints() == 2)
 			P2res = "Thirty";
-		if (p2Score == 3)
+		if (playerTwo.getPoints() == 3)
 			P2res = "Forty";
-		if (p1Score == 1)
+		if (playerOne.getPoints() == 1)
 			P1res = "Fifteen";
-		if (p1Score == 2)
+		if (playerOne.getPoints() == 2)
 			P1res = "Thirty";
 		score = P1res + "-" + P2res;
 	}
 
-	if (p1Score > p2Score && p2Score >= 3)
+	if (playerOne.getPoints() > playerTwo.getPoints() && playerTwo.getPoints() >= 3)
 	{
 		score = "Advantage player1";
 	}
 
-	if (p2Score > p1Score && p1Score >= 3)
+	if (playerTwo.getPoints() > playerOne.getPoints() && playerOne.getPoints() >= 3)
 	{
 		score = "Advantage player2";
 	}
 
-	if (p1Score >= 4 && p2Score >= 0 && (p1Score - p2Score) >= 2)
+	if (playerOne.getPoints() >= 4 && playerTwo.getPoints() >= 0 && (playerOne.getPoints() - playerTwo.getPoints()) >= 2)
 	{
 		score = "Win for player1";
 	}
-	if (p2Score >= 4 && p1Score >= 0 && (p2Score - p1Score) >= 2)
+	if (playerTwo.getPoints() >= 4 && playerOne.getPoints() >= 0 && (playerTwo.getPoints() - playerOne.getPoints()) >= 2)
 	{
 		score = "Win for player2";
 	}
